e9e176879addbd927b96f345bfa178b8c6433783
Joey Hejna <joey.hejna@gmail.com>
2025-01-10 16:41:25 -0800

    move away from commas in sweep names

--- tools/utils.py
+++ tools/utils.py
@@ -101,7 +101,7 @@ def load_ml_collections_sweep(path):
     sweep = {k: [str(vv) for vv in v] for k, v in sweep.items()}
     config_strs = list(itertools.product(*(v for v in sweep.values())))
     names = [
-        ",".join([k + "=" + _format_name(v) for k, v in zip(sweep.keys(), config_str, strict=False)])
+        "_".join([k + "-" + _format_name(v) for k, v in zip(sweep.keys(), config_str, strict=False)])
         for config_str in config_strs
     ]
     config_strs = [",".join(config_str) for config_str in config_strs]
