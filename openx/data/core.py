b7aa4b58834edcf02bbc657a6e27063d3135c7db
Joey Hejna <joey.hejna@gmail.com>
2025-03-27 20:28:20 +0000

    add step_idx permanently to load_dataset

--- openx/data/core.py
+++ openx/data/core.py
@@ -153,7 +153,7 @@ def load_dataset(
         if "episode_metadata" in ep:
             metadata = tf.nest.map_structure(lambda x: tf.repeat(x, ep_len), ep["episode_metadata"])
             steps["episode_metadata"] = metadata
-
+        steps["step_idx"] = tf.range(ep_len, dtype=tf.int32)
         steps = standardization_transform(steps)  # Standardize the episode
 
         if structure is not None:
